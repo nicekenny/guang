cf62fd98afa79b6fa06bf2c8e48ca5faa550d553
nicekenny <nicekenny@163.com>
2019-05-21 15:47:44 +0800

    edit

--- js/services.js
+++ js/services.js
@@ -80,14 +80,14 @@ $(function() {
 			}
 
 			// 固定导航条
-			var category_list = $("#category_list");
+			var category_box = $("#category_box");
 			if(window_top>45) {
-				if(!category_list.hasClass("nav_fixed")) {
-					category_list.addClass("nav_fixed");
+				if(!category_box.hasClass("nav_fixed")) {
+					category_box.addClass("nav_fixed");
 				}
 			} else {
-				if(category_list.hasClass("nav_fixed")) {
-					category_list.removeClass("nav_fixed");
+				if(category_box.hasClass("nav_fixed")) {
+					category_box.removeClass("nav_fixed");
 				}
 			}
 
