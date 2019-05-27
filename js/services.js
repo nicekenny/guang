fcb86e88531af8b1ed62f785e7ca80361d4ff857
nicekenny <nicekenny@163.com>
2019-05-27 19:42:48 +0800

    Update services.js

--- js/services.js
+++ js/services.js
@@ -527,7 +527,7 @@ function doBuy(a) {
 		clipboard_buy.on("success", function(e) {
 			// 拷贝成功
 			$(tpwd_dgContent).find(".tpwd_info").html("<span style=\"color:#FF6570;\">口令已拷贝</span>，打开"+userType_txt+"APP购买");
-			$(tpwd_dgContent).find(".tpwd_content").css("border", "1px dashed #66CC33").css("background-color", "#f7fff1");
+			$(tpwd_dgContent).find(".tpwd_content").css("border", "1px dashed #66CC33").css("background-color", "#FBFBFB");
 		});
 		clipboard_buy.on("error", function(e) {
 			// 提示失败，手工拷贝
