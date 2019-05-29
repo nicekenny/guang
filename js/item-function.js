84a4d68ddc959e8fb6986569b59854c32c6767c5
nicekenny <nicekenny@163.com>
2019-05-29 11:09:25 +0800

    Update item-function.js

--- js/item-function.js
+++ js/item-function.js
@@ -120,7 +120,7 @@ $(function() {
 					success: function (data) {
 						$("#tao_pwd_view").text(data);
 						var tpwd = data.replace(/￥/g,"");
-						var doQrCodeUrl = guangUrl("tpwd.html?id="+global_item_id+"&pwd="+tpwd+"&"+property_gss+"=item&pic="+picUrl);
+						var doQrCodeUrl = guangUrl("tpwd.html?id="+global_item_id+"&pwd="+tpwd+"&"+property_gss+"=item"); //&pic=picUrl
 						var qr_code_url = "http://qr.topscan.com/api.php?bg=ffffff&el=l&w=100&m=5&text="+encodeURIComponent(doQrCodeUrl);
 						$(".qr_code_img").attr("src",qr_code_url);
 						$("#item_share_text").val(item_share_text+"复制本条("+tpwd+")去打开购物APP即可把我带回家。");
