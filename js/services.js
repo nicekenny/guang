5e3d4011b45856f414fa35af7ebead10afc39a95
nicekenny <nicekenny@163.com>
2019-06-28 20:47:18 +0800

    Update services.js

--- js/services.js
+++ js/services.js
@@ -887,7 +887,7 @@ function openDownloadApp() {
 	var app_html = "<div class=\"dialog_box\">"
 		+"<div class=\"app_img\"><a href=\""+guangUrl("apk/guang_stable.apk")+"\"><img src=\""+guangUrl("images/app_logo.png")+"\"/></a></div>"
 		+"<div class=\"dbc_info\"><b>逛街啦APP</b></div>"
-		+"<div class=\"dbc_info\">点击上方图标下载APP到手机。</div>"
+		+"<div class=\"dbc_info\">点击上方图标下载逛街啦APP到手机。</div>"
 		+"<div class=\"button_links\">"
 		+"<a id=\"app_download_dialog_close\" class=\"one_center\">~不了，以后再说~</a>"
 		+"</div></div>";
